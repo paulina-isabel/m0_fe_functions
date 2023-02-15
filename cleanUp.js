064bcd847ee6be947aedc90c85dec9ee271253b2
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-02-14 22:08:47 -0600

    Add revisions to cleanup file syntax

--- cleanUp.js
+++ cleanUp.js
@@ -21,34 +21,40 @@ askForName()
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
+  var sum = first + second + third
+  return sum;
 }
 
 console.log(addThreeNums(1, 2, 3));
 console.log(addThreeNums(4, 2, 7));
+
 // Added console.log to see values printed to console
+/// indented code block
 
 // EX 3:
-function makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
-//function needed to be spelled out all the way: 'func' => 'function'
+// function needed to be spelled out all the way: 'func' => 'function'
+/// repositioned closing curly bracket to a new line after the code block
+
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
 
-return avg;
-  }
-  console.log (average(100, 50))
+  return avg;
+}
+console.log (average(100, 50))
 
-  // added console.log with calls with two numbers to confirm the code works as intended
+// added console.log with calls to two numbers to confirm the code works as intended
+// indented the code block lines the same amount
+// repositioned opening curly bracket to the same line as function and parameters
